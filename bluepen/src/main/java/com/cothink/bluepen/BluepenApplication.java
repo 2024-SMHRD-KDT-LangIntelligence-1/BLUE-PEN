a2e341955748b74aa5900f6d04451336aeb3cf1d
= <=>
2025-03-06 16:34:37 +0900

    Initial commit

---
+++ bluepen/src/main/java/com/cothink/bluepen/BluepenApplication.java
@@ -0,0 +1,13 @@
+package com.cothink.bluepen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluepenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluepenApplication.class, args);
+	}
+
+}
